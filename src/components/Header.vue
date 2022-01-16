c1b1111f6e3d8534297c7e41fd1bc9593ea023ab
AlexLeonkov <aleonkov2001@gmail.com>
2022-01-16 19:16:35 +0300

    tack-tracker

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-clicked="$emit('btn-clicked')"
+      :text="btnShown ? 'Close' : 'Add task'"
+      :color="btnShown ? 'red' : 'green'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    btnShown: Boolean,
+  },
+  data() {
+    return {
+      text: "Add task",
+    };
+  },
+  components: {
+    Button,
+  },
+  emits: ["btn-clicked"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
